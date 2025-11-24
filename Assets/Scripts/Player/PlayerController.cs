696f6943aa13ad7cbdf0e9d29324f8866c2e186e
Jayden2212 <jaydenjung@outlook.com>
2025-11-23 21:27:07 -0500

    Fixed Jumping

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -38,7 +38,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // ground check (casts a ray from current pos downwards, checks if it hits something)
-        // grounded = groundTouch == true;
+        grounded = groundTouch == true;
         // grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
 
         // handle drag
