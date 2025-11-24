3485d3e08b9ecce939275a185edd8e3dee6a8b68
Jayden2212 <jaydenjung@outlook.com>
2025-11-23 23:53:02 -0500

    Fixed Menu Screen

--- Assets/Scripts/UI/StartGame.cs
+++ Assets/Scripts/UI/StartGame.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    public void Start()
+    public void PlayStart()
     {
         SceneManager.LoadScene("GameScene");
     }
